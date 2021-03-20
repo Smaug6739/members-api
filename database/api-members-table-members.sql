29ddc7f43b23e594548eb562032dd1d83852ba09
Raphael <veauville.raphael@gmail.com>
2021-03-20 20:53:31 +0100

    Create db files template

--- database/api-members-table-members.sql
+++ database/api-members-table-members.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: members-api
+-- Host: 127.0.0.1    Database: instant-chat
 -- ------------------------------------------------------
 -- Server version	8.0.23
 
@@ -38,16 +38,6 @@ CREATE TABLE `members` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `members`
---
-
-LOCK TABLES `members` WRITE;
-/*!40000 ALTER TABLE `members` DISABLE KEYS */;
-INSERT INTO `members` VALUES (2,'Smaug',1,1,'default.png','$2b$10$b8RCKoLwcdGtdpTn3oIG/ubtTiCncJtUnPy1DZfi3QzKZ1jEun3uO','first name','last name','1','06 00 00 00 00','test@mail.com',1615924296609),(3,'Smaug',1,1,'default.png','$2b$10$BwlOLBgq1b.tmOzl6Sw2qOr1b34.VigAbKN2pmDdcoRQZOlA1caMa','first name','last name','sss','06 00 00 00 00','test@mail.com',1615924339184),(4,'Smaug',1,1,'default.png','$2b$10$j8.UNg/s4Rh9XpysXteeRerCmcuEFtTDw6N6iDw7KAekNUONV5fUO','first name','last name','sss','06 00 00 00 00','test@mail.com',1615924418608),(5,'Smaug',1,1,'default.png','$2b$10$vVzNX36Ml0VVx0U1Fbio5ujPX4xpq0VpPIDTSLucYxXUl2q6ic4nm','first name','last name','sss','06 00 00 00 00','test@mail.com',1615924440104),(6,'Smaug',1,1,'default.png','$2b$10$rJ77Z0SyTKxa4N7aQd4mR.h7ozEMAYdlbuDg4191Yx2QALx823KBq','first name','last name','1','06 00 00 00 00','test@mail.com',1615925128180);
-/*!40000 ALTER TABLE `members` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -58,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-03-20 20:48:42
+-- Dump completed on 2021-03-20 20:52:10
